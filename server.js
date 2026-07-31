da52937a6df59e5e4256107ae0cdd38db05813ea
victorargenal <vgallardoa@miumg.edu.gt>
2026-07-30 20:59:10 -0600

    test

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.get("/", (req, res) => {
 });
 
 
+
+
 require("./app/routes/cliente.routes")(app);
 require("./app/routes/proveedor.routes")(app);
 require("./app/routes/producto.routes")(app);
